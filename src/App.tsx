4dd6b8d18661ac89e492edbf87c0ec21e3eb6e47
Dario Reis <guttyerresjunior1995@gmail.com>
2022-08-25 14:50:04 -0300

    feat: adicionando tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from "@emotion/react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routers";
+import { LightTheme } from './shared/themes/Light';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
-
+};
