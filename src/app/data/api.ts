f190dffa166ea300a0f4cddf9fc6810261c547c4
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-20 07:08:40 -0300

    Feat: Product Fetch on Home

--- src/app/data/api.ts
+++ src/app/data/api.ts
@@ -1,6 +1,9 @@
-import {env} from '@/env'
+import { env } from '../env'
 
-export function api() {
+export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  return url
+  const apiPrefix = '/api'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
+
+  return fetch(url, init)
 }
