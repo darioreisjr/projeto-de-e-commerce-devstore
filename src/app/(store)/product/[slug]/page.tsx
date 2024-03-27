6a04950de60a48874507596a35bedfc6782ffacb
Dario Reis <guttyerresjunior1995@gmail.com>
2024-03-27 05:40:33 -0300

    Feat: Adding to cart

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/app/components/add-to-cart-button'
 import { api } from '@/app/data/api'
 import { Product } from '@/app/data/types/product'
 import { Metadata } from 'next'
@@ -73,6 +74,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            Em até 12x s/ juros de {''}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
@@ -111,12 +113,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { Search, ShoppingBag } from 'lucide-react'
+import { Search } from 'lucide-react'
+import { CartWidget } from './cart-widget'
 
 export function Header() {
   return (
@@ -21,10 +22,7 @@ export function Header() {
       </form>
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-4">
-          <div className="flex items-center gap-2">
-            <ShoppingBag className="h-4 w-4" />
-            <span className="text-sm">Cart (0)</span>
-          </div>
+          <CartWidget />
         </div>
 
         <div className="w-px h-4 bg-zinc-700" />
